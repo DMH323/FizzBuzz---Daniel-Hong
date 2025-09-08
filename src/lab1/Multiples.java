d4543ea3686b674944b3d91b561cc7dd1c6bcbe1
DMH323 <dani.hong@mail.utoronto.ca>
2025-09-08 10:43:12 -0400

    Updated code

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,15 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int multiples = 0;
+        for(int i=1;i<1000;i++){
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+            if(divisibleBy3||divisibleBy5){
+                multiples++;
+            }
+        }
+        System.out.println(multiples);
+    }
+}
