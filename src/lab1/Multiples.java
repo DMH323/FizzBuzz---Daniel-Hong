d0a8636e6ce0e20272053822e8f03a16fb0fff20
DMH323 <dani.hong@mail.utoronto.ca>
2025-09-08 10:44:53 -0400

    Updated code

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,14 +2,18 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        int multiples = 0;
+        multiples();
+    }
+
+    private static void multiples() {
+        int currentmultiples = 0;
         for(int i=1;i<1000;i++){
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
             if(divisibleBy3||divisibleBy5){
-                multiples++;
+                currentmultiples++;
             }
         }
-        System.out.println(multiples);
+        System.out.println(currentmultiples);
     }
 }
